821cfb57fe27ab2ad03116e26b41ef91fa805e00
KurakinPavel <pu.kurakin@yandex.ru>
2024-12-25 14:56:40 +0300

    Registration service: add openfeign Review 2

--- src/main/java/ru/practicum/workshop/registrationservice/client/EventClient.java
+++ src/main/java/ru/practicum/workshop/registrationservice/client/EventClient.java
@@ -4,9 +4,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
+import ru.practicum.workshop.registrationservice.client.config.CustomFeignClientConfiguration;
 import ru.practicum.workshop.registrationservice.client.dto.EventResponse;
 
-@FeignClient(name = "event-service-client", url = "http://host.docker.internal:8082")
+@FeignClient(name = "event-service-client", url = "http://host.docker.internal:8082",
+        configuration = CustomFeignClientConfiguration.class
+)
 public interface EventClient {
     @GetMapping("/events/{id}")
     EventResponse getEvent(@PathVariable Long id,
--- src/main/java/ru/practicum/workshop/registrationservice/client/UserClient.java
+++ src/main/java/ru/practicum/workshop/registrationservice/client/UserClient.java
@@ -6,10 +6,13 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import ru.practicum.workshop.registrationservice.client.config.CustomFeignClientConfiguration;
 import ru.practicum.workshop.registrationservice.dto.NewUserDto;
 import ru.practicum.workshop.registrationservice.client.dto.UpdateUserFromRegistrationDto;
 
-@FeignClient(name = "user-service-client", url = "http://host.docker.internal:8081")
+@FeignClient(name = "user-service-client", url = "http://host.docker.internal:8081",
+        configuration = CustomFeignClientConfiguration.class
+)
 public interface UserClient {
     @PostMapping("/users/internal")
     Long autoCreateUser(@RequestBody NewUserDto newUserDto);
